7cbc5cc58f9afa91d879d60fd62ce16e7a746eea
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-10-22 13:48:47 +0200

    CreatePortfolio function

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR.Entities;
 
 namespace VaR
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
