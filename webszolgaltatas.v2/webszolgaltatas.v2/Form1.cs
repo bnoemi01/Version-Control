4ab13cb2d4413d93a5b019a6d9b0b5c922b2cc97
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-10-28 13:22:27 +0200

    call web service

--- webszolgaltatas.v2/webszolgaltatas.v2/Form1.cs
+++ webszolgaltatas.v2/webszolgaltatas.v2/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using webszolgaltatas.v2.MnbServiceReference;
 
 namespace webszolgaltatas.v2
 {
     public partial class Form1 : Form
     {
+        MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+
         }
+
+
+        private void GetExchangeRates()
+        {
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+        }
+
+
     }
 }
