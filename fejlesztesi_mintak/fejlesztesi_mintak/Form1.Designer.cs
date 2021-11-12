cfc95b83d16ff4bbfccd2ddbe1c9d92891a8dc91
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-11-12 17:08:32 +0100

    Buttons for choosing between products.

--- fejlesztesi_mintak/fejlesztesi_mintak/Form1.Designer.cs
+++ fejlesztesi_mintak/fejlesztesi_mintak/Form1.Designer.cs
@@ -33,6 +33,9 @@ namespace fejlesztesi_mintak
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.buttonCar = new System.Windows.Forms.Button();
+            this.buttonBall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -54,15 +57,48 @@ namespace fejlesztesi_mintak
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // buttonCar
+            // 
+            this.buttonCar.Location = new System.Drawing.Point(125, 12);
+            this.buttonCar.Name = "buttonCar";
+            this.buttonCar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCar.TabIndex = 1;
+            this.buttonCar.Text = "CAR";
+            this.buttonCar.UseVisualStyleBackColor = true;
+            this.buttonCar.Click += new System.EventHandler(this.buttonCar_Click);
+            // 
+            // buttonBall
+            // 
+            this.buttonBall.Location = new System.Drawing.Point(244, 12);
+            this.buttonBall.Name = "buttonBall";
+            this.buttonBall.Size = new System.Drawing.Size(75, 23);
+            this.buttonBall.TabIndex = 2;
+            this.buttonBall.Text = "BALL";
+            this.buttonBall.UseVisualStyleBackColor = true;
+            this.buttonBall.Click += new System.EventHandler(this.buttonBall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coming next:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBall);
+            this.Controls.Add(this.buttonCar);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +107,9 @@ namespace fejlesztesi_mintak
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button buttonCar;
+        private System.Windows.Forms.Button buttonBall;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- fejlesztesi_mintak/fejlesztesi_mintak/Form1.cs
+++ fejlesztesi_mintak/fejlesztesi_mintak/Form1.cs
@@ -16,6 +16,8 @@ namespace fejlesztesi_mintak
     {
         private List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         private IToyFactory _factory;
         public IToyFactory Factory
         {
@@ -53,6 +55,26 @@ namespace fejlesztesi_mintak
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void buttonCar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void buttonBall_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
