aa570f80e27fa1efc1c4ba99352492631c2fdafd
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-11-12 16:53:46 +0100

    Create CarFactory

---
+++ fejlesztesi_mintak/fejlesztesi_mintak/Abstarctions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fejlesztesi_mintak.Abstarctions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
--- fejlesztesi_mintak/fejlesztesi_mintak/Entities/CarFactory.cs
+++ fejlesztesi_mintak/fejlesztesi_mintak/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace fejlesztesi_mintak.Entities
 {
-    public class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
