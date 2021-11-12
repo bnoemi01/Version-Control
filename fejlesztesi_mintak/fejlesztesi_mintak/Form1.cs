e2eacfbb807417c8e94af024eacb8b8731229bd8
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-11-12 17:02:59 +0100

    Update of Form1

--- fejlesztesi_mintak/fejlesztesi_mintak/Form1.cs
+++ fejlesztesi_mintak/fejlesztesi_mintak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using fejlesztesi_mintak.Entities;
+﻿using fejlesztesi_mintak.Abstarctions;
+using fejlesztesi_mintak.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,10 @@ namespace fejlesztesi_mintak
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -30,27 +31,27 @@ namespace fejlesztesi_mintak
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            Ball ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            Toy toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
