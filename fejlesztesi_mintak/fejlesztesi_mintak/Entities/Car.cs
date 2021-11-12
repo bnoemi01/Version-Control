07085d5b8d9e316e01612497af9649740843fafa
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-11-12 16:48:03 +0100

    Create car

--- fejlesztesi_mintak/fejlesztesi_mintak/Entities/Car.cs
+++ fejlesztesi_mintak/fejlesztesi_mintak/Entities/Car.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using fejlesztesi_mintak.Abstarctions;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace fejlesztesi_mintak.Entities
 {
-    class Car
+    public class Car : Toy
     {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
     }
 }
