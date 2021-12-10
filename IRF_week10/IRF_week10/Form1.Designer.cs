4d2e165b5795dfe21e8a7c3304438e7d55a508ba
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-12-10 13:49:30 +0100

    Choose winner

--- IRF_week10/IRF_week10/Form1.Designer.cs
+++ IRF_week10/IRF_week10/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 82);
+            this.label1.Location = new System.Drawing.Point(12, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(552, 49);
+            this.button1.Location = new System.Drawing.Point(15, 56);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 49);
             this.button1.TabIndex = 1;
